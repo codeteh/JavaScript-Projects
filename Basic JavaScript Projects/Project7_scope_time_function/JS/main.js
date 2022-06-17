5af387dc62e8ebcf410943a007d918acbf178cba
codeteh <101430666+codeteh@users.noreply.github.com>
2022-06-16 21:29:21 -0400

    Add files via upload

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,3 +1,6 @@
+/*scope is the functions, variables, and objects you have 
+access to. Variable A is global so all functions can use
+ it, variable B is local so only some functions can use it*/
 A = 10
 function Mult_numbers_1 () {
 	var B = 0.5;
@@ -10,17 +13,17 @@ function Add_numbers_1() {
 	document.write((A + 90) +"<br>");
 	document.write(A + B) +"<br>"; 
 	console.log((A + 90) +"<br>");
-	console.log(A + B) +"<br>";
+	console.log(A + B) +"<br>";// variable B is not local to this function. if you open the console, you’ll see the error “X is not defined
 }
 
 
 if (1 < 2) {
 	document.write (" The left number is smaller than the number on the right. <br> ")
-}
+}//if function, conditional statement that specifies that a section of code is to be executed if a condition is true
 
 function get_Date() {
-	if (new Date().getHours() <23) {
-	document.getElementById("Greeting").innerHTML = "How are you today?";
+	if (new Date().getHours() >20) {
+	document.getElementById("Greeting").innerHTML = "Isn't it kind of late?";
 	}
 }
 

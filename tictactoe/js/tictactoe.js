f25066619aa4071bf83ec7b478e73a07752bf868
codeteh <101430666+codeteh@users.noreply.github.com>
2022-08-03 07:25:48 -0400

    Add files via upload

--- tictactoe/js/tictactoe.js
+++ tictactoe/js/tictactoe.js
@@ -65,3 +65,94 @@ function placeXOrO(squareNumber) {
         }
     }
 } 
+
+//this function parses the selectedSquares array to seach for win conditions 
+//drawLine() function is called to draw a line on sthe screen if the condition is met.
+function checkWinConditions() { 
+    // X 0, 1, 2 condition.
+    if (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50, 100, 558, 100) }
+    // X 3, 4, 5 condition.
+    else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50, 304, 558, 304) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 508, 558, 508) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100, 50, 100, 558) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304, 50, 304, 558) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508, 50, 508, 558) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 508, 510, 90) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100, 100, 520, 520) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50, 100, 558, 100) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50, 304, 558, 304) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('6OX', '7O', '8O')) { drawWinLine(50, 508, 558, 508) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100, 50, 100, 558) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304, 50, 304, 558) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508, 50, 508, 558) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 508, 510, 90) }
+    // X 6, 7, 8 condition.
+    else if (arrayIncludes('0O', '4O', '8O')) { drawWinLine(100, 100, 520, 520) }
+    // this condition checks for a tie. if none of the above conditions are met and 
+    //9squares are selected to code executes.
+    else if (selectedSquares.length >=9) {
+        //this function plays teh tie game sound.
+        audio('./media/tie.mp3');
+        //this function sets a .3 second timer before the resetGame is called.
+        setTimeout(function (){ resetGame(); }, 500)
+    }
+    //this function check if an array includes 3 strings. it is used to check for 
+    //each win condition.
+    function arrayIncludes(squareA, squareB, squareC) {
+        //these 3 variables will be used to check for 3 in a row.
+        const a = selectedSquares.includes(squareA);
+        const b = selectedSquares.includes(squareB);
+        const c = selectedSquares.includes(squareC);
+        //if the 3 variable we pass are all include in our array then
+        //true is returned and our else if condition executes the drawLine() function.
+        if (a === true && b === true && c === true) { return true; }
+    }
+}
+
+//this function makes our body element temporarily unclicable.
+function disableClick() {
+    //this makes our body unclickable
+    body.style.pointerEvents = 'none';
+    //this makes our body clicable again after 1 second.
+    setTimeout(function () { body.style.pointerEvents = 'auto';}, 1000);
+}
+
+//this function takes a string parameter of the path you set ealier for 
+//placement sound ('./medi/place.mp3')
+function audio(audioURL) {
+    //we create a new audio object and we pass the path as a pameter
+    let audio = new Audio(audioURL);
+    //play method plays our audio sound.
+    audio.play();
+}
+
+//this function utilizes html canvas to draw in lines.
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    //this line accesses our html canvas element.
+    const canvas = document.getElementById('win-lines');
+    //this line gives us access to method adn properties to use on canvas
+    const c = canvas.getContext('2d');
+    //this line indicates where the start of a lines y axis is 
+    let x1 = coordX1,
+        //this line indicates where the start of a lines y axis is.
+        y1 = coordY1
+        //this line indicates where the end of a lines x axis is 
+        x2 = coordX2,
+        //this lien indicates where the end of a lines x axis is
+        y2 = coordY2,
+        //this variable store temporary x axis data we update in our animation loop. 
+        x = x1, 
+}
